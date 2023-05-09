5af23ea274c7fb2af1ac4a4606bfa6bdb9614a2d
Arturo <arturorjach@gmail.com>
2023-05-09 23:59:04 +0200

    🎉 First commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+  ],
+  ignorePatterns: ['.eslintrc.js', '**/*.json'],
+  rules: {
+    'prettier/prettier': ['error', {
+      usePrettierrc: true
+    }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/indent': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'indent': 'off',
+  }
+}
