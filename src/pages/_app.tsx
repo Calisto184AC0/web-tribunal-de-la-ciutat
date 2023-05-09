63a6b42dcc8b4e757fafdf0383b3281f65fc5bc3
Arturo <arturorjach@gmail.com>
2023-05-10 00:14:45 +0200

    ⚰️ Remove initial code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Tribunal de la Ciutat</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
+
+export default App
